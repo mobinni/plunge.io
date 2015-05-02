cbcbe3e0d48d9138989572af6905923bf3132042
Mo Binni <mobinni@Mos-MacBook-Pro.local>
2015-05-02 04:12:16 +0200

    refactored to eventdriven approach

--- plunge/lib/db/document.js
+++ plunge/lib/db/document.js
@@ -4,6 +4,7 @@ module.exports.bind = (function (schema) {
    Document.save(schema);
 });
 
+// TODO: implement eventdriven approach
 Document.save = (function (schema) {
     schema.methods.save = (function (obj, callback) {
       obj.save(function (err) {
